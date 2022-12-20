34ce2f3f84982abd01e4b8189cef5a608cb123c1
Piiykyk <pmtzengeni@gmail.com>
2022-12-19 18:01:12 -0800

    Task 2 Correction

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,12 @@
  * _strlen - returns the length of a string
  * @s: char to check
  * Description: this will return the length of a string
- * Return 0
+ * Return: 0
  */
 
 int _strlen(char *s)
 {
-	int a = 0;
+	int a = 0; /* start the counter from 0*/
 
 	for (; *s++;)
 		a++;
